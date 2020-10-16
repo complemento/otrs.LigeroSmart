8c2bb59c79e97df9c606186702137db27f6eabbc
Ronaldo Richieri <richieri@gmail.com>
2020-10-15 22:07:39 -0300

    Split from vuetify branch, focusing on LigeroIntegration

---
+++ Kernel/Modules/IntegrationExample2.pm
@@ -0,0 +1,143 @@
+
+package Kernel::Modules::IntegrationExample2;
+
+use strict;
+use warnings;
+
+use vars qw($VERSION);
+$VERSION = qw($Revision: 1.20 $) [1];
+
+sub new {
+    my ( $Integrations, %Param ) = @_;
+
+    # allocate new hash for object
+    my $Self = {%Param};
+    bless( $Self, $Integrations );
+
+
+    #$Integrations  = $Kernel::OM->Get('Kernel::System::SubscriptionPlan');
+    $Self->{ValidObject} = $Kernel::OM->Get('Kernel::System::Valid');
+
+    return $Self;
+}
+
+sub _GetTemplateData {
+    my $LayoutObject = $Kernel::OM->Get('Kernel::Output::HTML::Layout');
+
+    my $IntegrationData = {
+        Template => $LayoutObject->Output(
+                TemplateFile => 'Vue/Integrations/Example2'
+            ),
+        DataStructure => {
+            e6 => 1
+        },
+        Enable => _CheckStatus()
+    };
+    return $IntegrationData;
+}
+
+sub _CheckStatus {
+    my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
+
+    my %Integrations = %{ $ConfigObject->Get('Ligero::Integrations') };
+
+    return $Integrations{ExampleIntegration2}->{Enable} eq '1'
+}
+
+sub Run {
+    my ( $Self, %Param ) = @_;
+    my $ParamObject = $Kernel::OM->Get('Kernel::System::Web::Request');
+    my $LayoutObject = $Kernel::OM->Get('Kernel::Output::HTML::Layout');
+    my $ConfigObject = $Kernel::OM->Get('Kernel::Config');
+    my $SysConfigObject = $Kernel::OM->Get('Kernel::System::SysConfig');
+
+    # ------------------------------------------------------------ #
+    # change
+    # ------------------------------------------------------------ #
+    if ( $Self->{Subaction} eq 'GetTemplateData' ) {
+
+        my $IntegrationData = _GetTemplateData();
+
+        my $JSON = $LayoutObject->JSONEncode(
+            Data => $IntegrationData,
+        );
+
+        return $LayoutObject->Attachment(
+            ContentType => 'application/json; charset=utf8',
+            Content     => $JSON || '',
+            Type        => 'inline',
+            NoCache     => 1,
+        );
+    }
+
+    # ------------------------------------------------------------ #
+    # GetTemplateData
+    # ------------------------------------------------------------ #
+    if ( $Self->{Subaction} eq 'SetEnable' ) {
+
+        my $Value = $ParamObject->GetParam( Param => 'Value' ) || '';
+
+        my %Integrations = %{ $ConfigObject->Get('Ligero::Integrations') };
+
+        $Integrations{ExampleIntegration2}->{Enable} = $Value;
+
+        my %Setting = $SysConfigObject->SettingGet(
+            Name    => 'Ligero::Integrations###ExampleIntegration2',
+            Default => 1,
+        );
+
+        my $ExclusiveLockGUID = $SysConfigObject->SettingLock(
+            UserID    => 1,
+            Force     => 1,
+            DefaultID => $Setting{DefaultID},
+        );
+
+        my $Success = $SysConfigObject->SettingUpdate(
+            Name              => 'Ligero::Integrations###ExampleIntegration2',
+            EffectiveValue    => $Integrations{ExampleIntegration2},
+            ExclusiveLockGUID => $ExclusiveLockGUID,
+            UserID            => 1,
+        );
+
+        $Success = $SysConfigObject->SettingUnlock(
+            UserID    => 1,
+            DefaultID => $Setting{DefaultID},
+        );
+
+        my %DeploymentResult = $SysConfigObject->ConfigurationDeploy(
+            Comments      => "Update Enable Flag",
+            UserID        => 1,
+            Force         => 1,
+            DirtySettings => ['Ligero::Integrations###ExampleIntegration2'],
+        );
+
+        my $JSON = $LayoutObject->JSONEncode(
+            Data => {
+                Result => 0,
+                Message => "Erro ao salvar"
+            },
+        );
+
+        return $LayoutObject->Attachment(
+            ContentType => 'application/json; charset=utf8',
+            Content     => $JSON || '',
+            Type        => 'inline',
+            NoCache     => 1,
+        );
+    }
+
+}
+
+sub _Edit {
+    my ( $Self, %Param ) = @_;
+
+    return 1;
+}
+
+sub _Overview {
+    my ( $Self, %Param ) = @_;
+
+    return 1;
+}
+
+1;
